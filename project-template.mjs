29951c60fcac249fa4879d8d90fa63cf77f9bf48
Laozi 老子 <97992269+rpurinton@users.noreply.github.com>
2025-06-14 10:23:13 -0400

    Updating Template

--- project-template.mjs
+++ project-template.mjs
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import 'dotenv/config';
 import { log, registerHandlers, registerSignals } from '@purinton/common';
-registerHandlers({ logger: log }); registerSignals({ logger: log });
-
-log.debug('Starting project...');
+registerHandlers({ log });
+registerSignals({ log });
+log.info('project-template Started');
