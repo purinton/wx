480a6c1da03644afad8c421f1999c48f6c5bacac
Laozi 老子 <97992269+rpurinton@users.noreply.github.com>
2025-06-16 17:29:57 -0400

    Update discord-template.mjs

--- discord-template.mjs
+++ discord-template.mjs
@@ -1,10 +1,14 @@
 #!/usr/bin/env node
 import 'dotenv/config';
-import { log, path, registerHandlers, registerSignals } from '@purinton/common';
+import { log, fs, path, pathUrl, registerHandlers, registerSignals } from '@purinton/common';
 import { createDiscord } from '@purinton/discord';
 
 registerHandlers({ log });
 registerSignals({ log });
+
+const name = 'discord-template';
+const packageJson = JSON.parse(fs.readFileSync(path(import.meta, 'package.json')), 'utf8');
+const version = packageJson.version;
 
 (async () => {
     const client = await createDiscord({
@@ -16,10 +20,12 @@ registerSignals({ log });
             MessageContent: true,
         },
         context: {
-            presence: { activities: [{ name: 'example', type: 4 }], status: 'online' },
+            presence: { activities: [{ name: `${name} v${version}`, type: 4 }], status: 'online' },
         },
     });
-    registerSignals({ shutdownHook: async () => {
-        await client.destroy();
-    }});
+    registerSignals({
+        shutdownHook: async () => {
+            await client.destroy();
+        }
+    });
 })();
