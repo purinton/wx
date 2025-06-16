7e985ee8c2b59f4246125eff194d1ddca23ec4a1
Russell Purinton <russell.purinton@gmail.com>
2025-06-16 18:22:57 +0000

    Pushed 2025-06-16 18:22:57

--- tests/discord-template.test.mjs
+++
@@ -1,6 +0,0 @@
-import { jest } from '@jest/globals';
-test('discord-template.mjs can be imported without error', async () => {
-  process.env.LOG_LEVEL = 'none';
-  await import('../discord-template.mjs');
-  expect(true).toBe(true);
-});
